17b097c4b1ba24bec8251e7aead6163cf7bc8033
t-yng <t.yng.greenmonkey@gmail.com>
2019-07-24 01:53:30 +0900

    indexファイルに既にファイルパスが存在する時はハッシュ値を置き換える

--- src/index-file.ts
+++ src/index-file.ts
@@ -1,5 +1,11 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import * as os from 'os';
+
+interface IndexEntry {
+    hash: string,
+    file: string
+}
 
 export default class Index {
     private _file: string;
@@ -8,8 +14,32 @@ export default class Index {
         this._file = path.join('.mygit', 'index');
     }
 
+    get entries(): IndexEntry[] {
+        if(!fs.existsSync(this._file)) return [];
+
+        const content = fs.readFileSync(this._file).toString();
+        const lines = content.split(os.EOL);
+        return lines.map( (line) => {
+            const [ hash, file ] = line.split(' ');
+            return { hash, file };
+        });
+    }
+
     add = (hash: string, file: string): void => {
-        const entry = `${hash} ${file}\0`;
-        fs.appendFileSync(this._file, entry);
+        const entries = this.entries;
+        const index = entries.findIndex( entry => entry.file === file );
+        if(index > -1) {
+            entries[index].hash = hash;
+            this.write(entries);
+        } else {
+            let newEntry = `${hash} ${file}`;
+            if(entries.length > 0) newEntry += os.EOL;
+            fs.appendFileSync(this._file, newEntry);
+        }
+    }
+
+    private write = (entries: IndexEntry[]) => {
+        const lines = entries.map( entry => `${entry.hash} ${entry.file}`);
+        fs.writeFileSync(this._file, lines.join(os.EOL));
     }
 }
