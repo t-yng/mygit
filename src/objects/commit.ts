bea9e3a8a234c0642d1a79b32517af7cc1e4528a
t-yng <t.yng.greenmonkey@gmail.com>
2019-07-26 09:07:31 +0900

    コミットオブジェクトの作成日時を追加

--- src/objects/commit.ts
+++ src/objects/commit.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { sha1 } from './hash';
 import * as os from 'os';
 import MyGitObject from './object';
@@ -6,18 +7,19 @@ import { Index } from '../index-file';
 interface Meta {
     tree: string,
     parent?: string,
-    author?: string,
-    committer?: string,
+    author: string,
+    committer: string,
     message: string
 }
 
 export default class Commit extends MyGitObject {
     static create = (meta: Meta): Commit => {
+        const timestamp = moment().format('X ZZ');
         const content = [
             `tree ${meta.tree}`,
             (meta.parent != null) ? `parent ${meta.parent}` : '',
-            (meta.author != null) ? `author ${meta.author}` : '',
-            (meta.committer != null) ? `commiter ${meta.committer}` : '',
+            (meta.author != null) ? `author ${meta.author} ${timestamp}` : '',
+            (meta.committer != null) ? `commiter ${meta.committer} ${timestamp}` : '',
             `${os.EOL}${meta.message}`
         ]
         .filter( str => str !== '')
--- src/objects/index.ts
+++ src/objects/index.ts
@@ -35,9 +35,16 @@ export const createCommit = (index: Index, message: string): Commit => {
   const tree = createTree(index.toTree().root.children);
   const parent = refs.parentCommitHash;
 
+  // TODO: 設定値から取得するようにする
+  //       設定は未実装なので仮の値を設定しておく
+  const author = 'mygit <mygit@test.com>';
+  const committer = author;
+
   const commit = Commit.create({
     tree: tree.hash,
     parent: parent,
+    author: author,
+    committer: committer,
     message: message,
   });
 
